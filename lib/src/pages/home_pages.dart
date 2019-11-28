d9a133a8d19e37a8ad5475f54dbf37abb51a7a6d
IsaiasPC <isamed92@gmail.com>
2019-11-28 00:19:09 -0300

    add swiper in home_page

--- lib/src/pages/home_pages.dart
+++ lib/src/pages/home_pages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -6,9 +7,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('hola mundo!!!!'),
+      appBar: AppBar(
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon( Icons.search),
+            onPressed: () {}
+          )
+        ]
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas()
+          ]
+        )
       )
     );
   }
+
+
+  Widget _swiperTarjetas(){
+    return Container(
+      width: double.infinity,
+      height: 300.0,
+      padding: EdgeInsets.only(top: 10.0),
+      child: Swiper(
+          itemBuilder: (BuildContext context,int index){
+            return Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          },
+          itemCount: 3,
+          itemWidth: 200.0,
+          layout: SwiperLayout.STACK,
+          // pagination: SwiperPagination(),
+          // control: SwiperControl(),
+        ),
+    );
+  }
+
 }
